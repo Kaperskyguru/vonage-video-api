cbc58e3ded4bab34c5e2ec035e704f2a23d03845
Solomon Eseme <kaperskyguru@gmail.com>
2020-12-10 23:08:31 +0100

    Added seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +15,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        {
+            DB::table('users')->insert([
+                'name' => 'Test Student',
+                'email' => 'teststudent@gmail.com',
+                'user_type' => 'Student',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+            ]);
+        }
     }
 }
